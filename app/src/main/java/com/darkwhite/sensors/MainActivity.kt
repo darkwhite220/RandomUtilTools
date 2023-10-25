593b531dd39f5b2ebdb48e27ebcddf3528f0f29b
darkwhite220 <53045980+darkwhite220@users.noreply.github.com>
2023-10-25 14:26:09 +0100

    Change formating

--- app/src/main/java/com/darkwhite/sensors/MainActivity.kt
+++ app/src/main/java/com/darkwhite/sensors/MainActivity.kt
@@ -17,9 +17,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.darkwhite.sensors.ui.theme.SensorsTheme
@@ -147,38 +151,32 @@ fun MainScreen(
     modifier: Modifier = Modifier,
 ) {
 //    val deviceSensors = remember { mutableListOf(sensorManager.getSensorList(Sensor.TYPE_ALL)) }
+    val sBuilder by remember(x,y,z) { derivedStateOf {
+        val (pitch, roll) = calculatePitchAndRoll(floatArrayOf(x, y, z))
+        val (pitch1, roll1) = calculatePitchAndRoll(floatArrayOf(x, z, y))
+        val (pitch2, roll2) = calculatePitchAndRoll(floatArrayOf(z, y, x))
+        """
+            X=$x
+            Y=$y
+            Z=$z
+            
+            ${(x / MAX_VALUE * 100).round(2)}%, ${(y / MAX_VALUE * 100).round(2)}%, ${(z /
+            MAX_VALUE * 100).round(2)}%
+            
+            Flat Leveled: ${isSurfaceLeveled(pitch, roll)}, ${pitch.round(2)}, ${roll.round(2)}
+            Up Leveled  : ${isSurfaceLeveled(pitch1, roll1)}, ${pitch1.round(2)}, ${roll1.round(2)}
+            Side Leveled: ${isSurfaceLeveled(pitch2, roll2)}, ${pitch2.round(2)}, ${roll2.round(2)}
+        """.trimIndent()
+    } }
+    
     Column(
         modifier = modifier.verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        verticalArrangement = Arrangement.Center
     ) {
-        
         if (accelerometer != null) {
-            Text(text = "X=$x, Y=$y, Z=$z")
-            Text(
-                text = "${(x / MAX_VALUE * 100).round(2)}%, " +
-                    "${(y / MAX_VALUE * 100).round(2)}%, " +
-                    "${(z / MAX_VALUE * 100).round(2)}%"
-            )
-            val (pitch, roll) = calculatePitchAndRoll(floatArrayOf(x, y, z))
-            Text(
-                text = "1) Surface Leveled: ${isSurfaceLeveled(pitch, roll)}, " +
-                    "${pitch.round(2)}, ${roll.round(2)}"
-            )
-            
-            val (pitch1, roll1) = calculatePitchAndRoll(floatArrayOf(x, z, y))
-            Text(
-                text = "2) Surface Leveled: ${isSurfaceLeveled(pitch1, roll1)}, " +
-                    "${pitch1.round(2)}, ${roll1.round(2)}"
-            )
-            
-            val (pitch2, roll2) = calculatePitchAndRoll(floatArrayOf(z, y, x))
-            Text(
-                text = "3) Surface Leveled: ${isSurfaceLeveled(pitch2, roll2)}, " +
-                    "${pitch2.round(2)}, ${roll2.round(2)}"
-            )
-            
+            Text(text = sBuilder, style = MaterialTheme.typography.titleLarge)
         } else {
-            Text(text = "No Gyroscope Available")
+            Text(text = "No Accelerometer Available")
         }
 
 //        Text(text = "Available Sensors", style = MaterialTheme.typography.titleLarge)
